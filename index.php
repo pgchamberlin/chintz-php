5f35c73e881a328f21941aaa5750bc48f71fcae7
pgchamberlin <pgchamberlin@googlemail.com>
2015-01-06 01:20:27 +0000

    Tidied up index

--- index.php
+++ index.php
@@ -5,22 +5,22 @@ require_once __DIR__.'/StaticLibraryCompositor.php';
 require_once __DIR__.'/FileSystemAliasLoader.php';
 
 $compositor = new StaticLibraryCompositor();
-$elementA = $compositor
-    ->prepare('elementA')
-    ->render(array(
-        'title' => 'Static Library Rendering Demo',
-        'subtitle' => 'Made up person',
-        'items' => array(
-            array(
-                'name' => 'bobbins',
-                'occupation' => 'philosopher'
-            ),
-            array(
-                'name' => 'bobbinella',
-                'occupation' => 'scientist'
-            )
+$compositor->prepare('elementA');
+
+$data = array(
+    'title' => 'Static Library Rendering Demo',
+    'subtitle' => 'Made up person',
+    'items' => array(
+        array(
+            'name' => 'bobbins',
+            'occupation' => 'philosopher'
+        ),
+        array(
+            'name' => 'bobbinella',
+            'occupation' => 'scientist'
         )
-    ));
+    )
+);
 
 ?>
 <!doctype html>
@@ -31,6 +31,6 @@ $elementA = $compositor
     </style>
 </head>
 <body>
-    <?= $elementA ?>
+    <?= $compositor->render($data) ?>
 </body>
 </html>
